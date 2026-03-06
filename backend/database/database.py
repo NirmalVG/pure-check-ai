07fca996aa4a8446d19d33c88f2b6b2c6d39a918
NIRMAL V G <Nirmalvg97@gmail.com>
2026-03-06 22:41:31 +0530

    Add render config

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,21 +1,14 @@
 import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # Loads .env file automatically
 
-user = os.getenv("DB_USER")
-password = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-db_name = os.getenv("DB_NAME")
-
-# Build the URL dynamically
-SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{user}:{password}@{host}/{db_name}"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
